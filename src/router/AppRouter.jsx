1e04156e6f2f9b60172a499cf667fb9a76ea57f2
Samuel Martinez Arreola <sma.2904@gmail.com>
2022-11-18 11:08:39 -0600

    creating public routes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,13 +3,19 @@ import { Routes, Route} from 'react-router-dom';
 import { LoginPage } from '../auth';
 import { HeroesRoutes } from '../heroes/routes/HeroesRoutes';
 import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
-        <Route path="login" element={<LoginPage/>}/>
-        <Route path="*" element = {
+        {/* <Route path="login" element={<LoginPage/>}/> */}
+        <Route path="/login" element={
+          <PublicRoute>
+            <LoginPage />
+          </PublicRoute>
+        } />
+        <Route path="/*" element = {
           <PrivateRoute>
             <HeroesRoutes/>
           </PrivateRoute>
